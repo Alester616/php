c6f7bf8dc69e76049711359c8057013e70aef9dc
Alester616 <marcusps3gta5@gmail.com>
2020-02-23 11:32:58 -0400

    resolvi o problema de conflito

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
  echo"Olá mundo 666";
 
- echo" olá udemy";
-=======
- echo"Olá mundo 123";
+ echo" olá udemy e hcode";
 
- echo" Olá hcode!";
->>>>>>> 2fee96bbb96d14a6c9bb455aec9a83e40c100e14
-
-?>
+ ?>
